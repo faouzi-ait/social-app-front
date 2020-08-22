689530bd054d7ec07f2e995ea36b2a9e2c77cac3
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2020-08-23 01:26:18 +0200

    Fixed redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -50,7 +50,7 @@ export const store = createStore(
   persistedReducer,
   compose(
     applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
